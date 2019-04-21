8af48f609ba009889085be21f02e1f1b224a8b31
timekeeper01 <46344493+timekeeper01@users.noreply.github.com>
2019-04-21 16:39:03 +0800

    Update getSubject.js

--- pages/getSubject/getSubject.js
+++ pages/getSubject/getSubject.js
@@ -13,15 +13,6 @@ Page({
     })
   },
 
-  goToJwxt: function () {
-    wx.navigateTo({
-      url: '../out/out', //
-      success: function () {
-      }, //成功后的回调；
-      fail: function () { }, //失败后的回调；
-      complete: function () { } //结束后的回调(成功，失败都会执行)
-    })
-  },
   /**
    * 生命周期函数--监听页面加载
    */
